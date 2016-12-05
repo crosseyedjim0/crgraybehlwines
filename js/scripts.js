2dcbcc97a0ca240ab94a0adc52efb2dac561e62d
Jim Cross <crosseyedjim0@yahoo.com>
2016-12-05 12:55:41 -0800

    reset commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function init() {
+  $(document).ready(function() {
+    $(".modal-wide").on("show.bs.modal", function() {
+  var height = $(window).height() - 200;
+  $(this).find(".modal-body").css("max-height", height);
+});
+  });
+}
+
+init();
